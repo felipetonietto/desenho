48dd6dd1f74a04e6736761786470ab11cca751a5
Felipe Tonietto <61717379+felipetonietto@users.noreply.github.com>
2020-03-24 17:28:28 -0300

    Add files via upload

--- etch-a-sketch-FINISHED.js
+++ etch-a-sketch-FINISHED.js
@@ -1,15 +1,14 @@
-// Select the elements on the page - canvas, shake button
+
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
 const MOVE_AMOUNT = 50;
-// Setup our canvas for drawing
-// make a variable called height and width from the same properties on our canvas.
+
 const { width, height } = canvas;
 
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
-// create random x and y starting points on the canvas
+
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
@@ -22,17 +21,17 @@ ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
-// write a draw function
+
 function draw({ key }) {
-  // increment the hue
+  
   hue += 1;
   console.log(hue);
   ctx.strokeStyle = `hsl(${Math.random() * 360}, 100%, 50%)`;
   console.log(key);
-  // start the path
+  
   ctx.beginPath();
   ctx.moveTo(x, y);
-  // move our x and y values depending on what the user did
+ 
   switch (key) {
     case 'ArrowUp':
       y -= MOVE_AMOUNT;
@@ -53,14 +52,14 @@ function draw({ key }) {
   ctx.stroke();
 }
 
-// write a handler for the keys
+
 function handleKey(e) {
   if (e.key.includes('Arrow')) {
     e.preventDefault();
     draw({ key: e.key });
   }
 }
-// clear /shke function
+
 function clearCanvas() {
   canvas.classList.add('shake');
   ctx.clearRect(0, 0, width, height);
